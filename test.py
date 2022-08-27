d1decf02604e1cf3afd3a2e4267b41c48a996943
Edwin John <111343130+EDWINS-JOHN@users.noreply.github.com>
2022-08-27 20:06:48 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,15 +1,2 @@
-<<<<<<< HEAD
-def main():
-   print("Hellow from kenya")
-   
-   
-if __name__=='__main__':
-   main()
-=======
-def main():
-   print("Hellow from kenya")
-   
-   
-if __name__=='__main__':
-   main()
->>>>>>> refs/remotes/origin/main
+
+
